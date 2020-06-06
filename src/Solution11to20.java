360a33faa5a508c9414e3bcca683e89a3ad25a32
swaggyTech <122125597@qq.com>
2020-06-06 21:34:20 +0800

    19.删除倒数第N个节点

--- src/Solution11to20.java
+++ src/Solution11to20.java
@@ -430,4 +430,41 @@ public class Solution11to20 {
         }
         return result;
     }
+
+    /**
+     * 19.删除倒数第N个节点
+     * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     * <p>
+     * 示例：
+     * <p>
+     * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+     * <p>
+     * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     * 说明：
+     * <p>
+     * 给定的 n 保证是有效的。
+     * <p>
+     * 进阶：
+     * <p>
+     * 你能尝试使用一趟扫描实现吗？
+     */
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode ret = new ListNode(-1);
+        ret.next = head;
+        ListNode l1 = ret, l2 = ret;
+        while (n > 0 && l2 != null) {
+            l2 = l2.next;
+            n--;
+        }
+        while (l2 != null) {
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (l1.next != null) {
+            l1.next = l1.next.next;
+            return ret.next;
+        } else {
+            return null;
+        }
+    }
 }
